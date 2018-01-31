3d43d9048ab9458dbaa605ee5efe1fef777ce75b
Chunle Tian <chuntian997@gmail.com>
2018-01-31 11:22:24 +0800

    不知道是啥，好久没看了

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var setting = new Dictionary<string, string> {
+                {"name","nidexioa"},
+                {"age","23" }
+            };
+
+            var builder = new ConfigurationBuilder()
+                .AddInMemoryCollection(setting)
+                .AddCommandLine(args);
+            var configuration = builder.Build();
+            Console.WriteLine($"name:{configuration["name"]}");
+            Console.WriteLine($"age:{configuration["age"]}");
+            Console.ReadLine();
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
